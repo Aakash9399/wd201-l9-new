20762ef8e49e7dfac6ff54639f08bce3f9e97d9d
Aakash9399 <akashpalliwar79@gmail.com>
2022-12-20 19:50:17 +0530

    some minor changes

--- models/todo.js
+++ models/todo.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Todo.belongsTo(models.User, {
-        foreignKey: "userId",
+        foreignKey: "userID",
       });
       // define association here
     }
